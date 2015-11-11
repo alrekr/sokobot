c31eb9465d40e07a532d531ab24fd4079280e8dd
Malthe Høj-Sunesen <malthe.s@gmail.com>
2015-11-11 13:36:11 +0100

    Created control structure and orientation tracker

--- src/common.h
+++ src/common.h
@@ -1,4 +1,4 @@
-#define KP 12.0
+#define KP 15.0
 #define KI 0.6
 #define KD 0.8
 #define TP 30.0
@@ -9,12 +9,19 @@
 #define LCD_POS2 64
 
 #define INIT 0
-#define BLACK_THRESHOLD 50
-#define CAREFUL_SPEED 75
+#define BLACK_THRESHOLD 40
+#define CAREFUL_SPEED 70
 #define FAST_SPEED 85
 #define SPEED_LIMIT 99
 #define MIN_SPEED 50
-#define TURN_SPEED 80
+#define TURN_SPEED 60
 #define CALIBRATION_TURN_SPEED 40
 
 #define CALIBRATION_TURN_TIME 10000
+
+#define ROUTE_NAME "sokoban_route.dat"
+
+#define UP 'u'
+#define DOWN 'd'
+#define LEFT 'l'
+#define RIGHT 'r'
